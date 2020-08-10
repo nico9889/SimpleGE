c7d0349dee2b58c7f7da53090c7b898364e2f0f2
Nicola <nx9889@gmail.com>
2020-08-10 18:58:45 +0200

    Updated test (Missing sprites!!!)

--- src/Test/SideScrolling/Main.java
+++ src/Test/SideScrolling/Main.java
@@ -11,6 +11,9 @@ import Gfx.Animation;
 import Gfx.Image;
 import Gfx.Sprite;
 import Physics.Entity;
+import Physics.Gravity;
+import Physics.Physics;
+import Test.SideScrolling.World.Terrain;
 
 import java.awt.event.KeyEvent;
 import java.io.File;
@@ -26,18 +29,25 @@ import java.util.Random;
 public class Main {
     static final Random rnd = new Random();
 
-    public static Image[] loadSprites(File folder) throws IOException {
+    public static Image[] loadSprites(File folder, double r, double g, double b){
         Image[] frames = new Image[(folder.listFiles().length)];
         int frame = 0;
-        for(File file:folder.listFiles()){
-            frames[frame] = new Image(file.getPath());
-            frame++;
+        try {
+            for (File file : folder.listFiles()) {
+                Image image = new Image(file.getPath());
+                image.recolor(r, g, b);
+                frames[frame] = image;
+                frame++;
+            }
+        }catch(IOException io){
+            io.printStackTrace();
+            System.exit(-1);
         }
         return frames;
     }
 
-    public static ArrayList<Sprite> clouds_gen() throws IOException {
-        Image[] cloud_frames = loadSprites(new File("resources/sprites/world/clouds"));
+    public static ArrayList<Sprite> clouds_gen(){
+        Image[] cloud_frames = loadSprites(new File("resources/sprites/world/clouds"), 1,1,1);
         ArrayList<Sprite> clouds = new ArrayList<>();
         for(int i=0;i<10;i++){
             Animation cloud_anim = new Animation(cloud_frames, Math.abs((rnd.nextInt()%30)+10));
@@ -46,16 +56,25 @@ public class Main {
         return clouds;
     }
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-        Engine engine = new Engine(1280,720, false, "Test Game");
+    public static void main(String[] args) throws IOException {
+        Engine engine = new Engine(1280,720,60,60, false, "Test Game");
+        Gravity g = new Gravity(0,0,1280,720, 0.0,3.0);
+        Physics donald_p = new Physics(2);
+        Physics ex_p = new Physics(3);
+
         Scene scene = new Scene("Test scene");
         Sprite bg = new Sprite("resources_sidescrolling/sprites/background.png", 0,0,0);
         Image t_image = new Image(new File("resources_sidescrolling/sprites/background2.png"));
-        Entity terrain = new Entity(new Animation(new Image[]{t_image},0), 0,0,1);
+        Terrain terrain = new Terrain(new Animation(new Image[]{t_image},0), 0,0,1);
 
-        Image[] idle_frames = loadSprites(new File("resources_sidescrolling/sprites/player/idle/"));
-        Animation idle = new Animation(idle_frames,2);
-        Player donald = new Player(idle, 100, 160, 1);
+        Image[] idle_frames_0 = loadSprites(new File("resources_sidescrolling/sprites/player/idle/"), 1,1,1);
+        Image[] idle_frames_1 = loadSprites(new File("resources_sidescrolling/sprites/player/idle/"), 2,0.5,0);
+        Animation idle_0 = new Animation(idle_frames_0,4);
+        Animation idle_1 = new Animation(idle_frames_1, 4);
+
+        Player donald = new Player(idle_0, 500, 560, 1, donald_p);
+        Player ex = new Player(idle_1, 100, 560, 1, ex_p);
+        ex.autoHitBox();
         donald.autoHitBox();
         terrain.autoHitBox();
         ArrayList<Sprite> clouds = clouds_gen();
@@ -63,21 +82,32 @@ public class Main {
         ArrayList<Entity> entities = new ArrayList<>();
         entities.add(donald);
         entities.add(terrain);
+        entities.add(ex);
         scene.bulkAddEntities(entities);
         scene.addSprite(bg);
-
+        scene.addGravity(g);
         engine.addScene(scene);
         engine.nextScene();
 
         KeyMap map = new KeyMap();
-        Action move_up = new Action((() -> donald.moveBy( 0,8)), donald::idle);
-        Action move_down = new Action((() -> donald.moveBy( 0,-8)), donald::idle);
-        Action move_left = new Action((() -> donald.moveBy( -8,0)), donald::idle);
-        Action move_right = new Action((() -> donald.moveBy( 8,0)), donald::idle);
+        Action move_up = new Action(() -> donald.moveBy( 0,20), donald::idle);
+        Action move_down = new Action(() -> donald.moveBy( 0,-8), donald::idle);
+        Action move_left = new Action(() -> donald.moveBy( -8,0), donald::idle);
+        Action move_right = new Action(() -> donald.moveBy( 8,0), donald::idle);
         map.addKey(KeyEvent.VK_UP, move_up);
         map.addKey(KeyEvent.VK_DOWN, move_down);
         map.addKey(KeyEvent.VK_LEFT, move_left);
         map.addKey(KeyEvent.VK_RIGHT, move_right);
+        Action second_move_up = new Action(() -> ex.moveBy( 0,20), ex::idle);
+        Action second_move_down = new Action(() -> ex.moveBy( 0,-8), ex::idle);
+        Action second_move_left = new Action(() -> ex.moveBy( -8,0), ex::idle);
+        Action second_move_right = new Action(() -> ex.moveBy( 8,0), ex::idle);
+        map.addKey(KeyEvent.VK_W, second_move_up);
+        map.addKey(KeyEvent.VK_S, second_move_down);
+        map.addKey(KeyEvent.VK_A, second_move_left);
+        map.addKey(KeyEvent.VK_D, second_move_right);
+
+
 
         engine.addKeyMap(map);
 
@@ -90,6 +120,7 @@ public class Main {
                 }
             }
         }, "Cloud Update");
+
         engine.start();
     }
 }
--- src/Test/SideScrolling/Player.java
+++ src/Test/SideScrolling/Player.java
@@ -3,38 +3,66 @@ package Test.SideScrolling;
 import Gfx.Animation;
 import Gfx.Image;
 import Physics.Entity;
+import Physics.Physics;
 
 import java.io.File;
-import java.io.IOException;
 
-public class Player extends Entity {
-    private final Image[] walk_left_anim = Main.loadSprites(new File("resources/sprites/player/run_left"));
-    private final Image[] walk_right_anim = Main.loadSprites(new File("resources/sprites/player/run_right"));
-    private final Animation walk_left = new Animation(walk_left_anim,2);
-    private final Animation walk_right = new Animation(walk_right_anim,2);
+public class Player extends Entity{
+    private final Image[] walk_left_anim = Main.loadSprites(new File("resources/sprites/sonic/run_left"), player,1./player,2-player);
+    private final Image[] walk_right_anim = Main.loadSprites(new File("resources/sprites/sonic/run_right"), player,1./player,2-player);
+    private final Image[] air_file = Main.loadSprites(new File("resources/sprites/sonic/air"), player,1./player,2-player);
+    private static int player = 1;
+    private final Animation walk_left = new Animation(walk_left_anim,1);
+    private final Animation walk_right = new Animation(walk_right_anim,1);
+    private final Animation air = new Animation(air_file, 1);
+    private boolean last_air = false;
     private final Animation idle;
+    private int air_distance = 0;
 
-    public Player(Animation idle, int x, int y, int z) throws IOException {
+    public Player(Animation idle, int x, int y, int z){
         super(idle, x, y, z);
         this.idle = idle;
+        player++;
+    }
+
+    public Player(Animation idle, int x, int y, int z, Physics p){
+        super(idle, x, y, z, p);
+        this.idle = idle;
+        player++;
     }
 
     @Override
-    public void moveBy(int dx, int dy){
-        if(dx>0)
+    public boolean moveBy(int dx, int dy){
+        if(air_distance>200 && dy>0)
+            dy=0;
+        checkHitbox(dx, dy);
+        if(dx>0 && !in_air)
             super.animation = walk_right;
-        else if(dx<0)
+        else if(dx<0 && !in_air)
             super.animation = walk_left;
-        collisions.clear(); // FIXME missing mutual exclusion, collisions cleared before reading
-        hb.moveBy(dx, dy);
-        for(Entity c:collidable){
-            if(hb.checkCollision(c.hb)) collisions.add(c);
+        else if(in_air) {
+            last_air=in_air;
+            super.animation = air;
         }
+        if(last_air!=in_air){
+            last_air=in_air;
+            idle();
+        }
+
+        if(in_air) {
+            if (dy > 0)
+                air_distance = air_distance + Math.abs(dx) + dy;
+        }
+        else
+            air_distance=0;
         if(collisions.isEmpty()){
-            super.moveBy(dx, dy);
+            x += dx;
+            y += dy;
+            return true;
         }
         else{
             hb.moveBy(-dx, -dy);
+            return false;
         }
     }
 
